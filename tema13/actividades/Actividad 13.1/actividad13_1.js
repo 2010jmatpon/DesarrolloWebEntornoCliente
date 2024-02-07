efad38a29e39936f66589b7afe2df46f66402627
2010jmatpon <jmatpon2010@g.educaand.es>
2024-02-07 13:09:58 +0100

    Commit

--- tema13/actividades/Actividad 13.1/actividad13_1.js
+++ tema13/actividades/Actividad 13.1/actividad13_1.js
@@ -1,5 +1,27 @@
 "use strict";
-class GrupoBot {
-}
-class Boton {
-}
+$(function () {
+    class Boton {
+        //solo constructor
+        constructor(boton) {
+            $('#contenido').append(`<button id="${boton}">${boton}</button>`);
+        }
+    }
+    class GrupoBot {
+        constructor() {
+            this.contador = 0;
+            console.log("grupobot");
+            $('#add').on('click', () => this.add());
+        }
+        add() {
+            this.contador++;
+            new Boton(this.contador);
+        }
+        rest() {
+            if (this.contador >= 1) {
+                $(`#boton${this.contador}`).remove();
+                this.contador--;
+            }
+        }
+    }
+    new GrupoBot();
+});
--- tema13/actividades/Actividad 13.1/actividad13_1.ts
+++ tema13/actividades/Actividad 13.1/actividad13_1.ts
@@ -1,11 +1,34 @@
-interface Bot{
-    
-}
+$(function () {
+  class Boton {
+    //solo constructor
+    constructor(boton:number) {
+        $('#contenido').append(`<button id="${boton}">${boton}</button>`);
+    }
+  }
 
-class GrupoBot implements Bot{
+  interface Bot {
+    //metodos add y rest
+    add(): void;
+    rest(): void;
+  }
 
-}
+  class GrupoBot implements Bot {
+    private contador: number = 0;
+    constructor() {
+        console.log("grupobot");
+        $('#add').on('click',()=>this.add());
+    }
+    add(): void {
+      this.contador++;
+      new Boton(this.contador);
+    }
+    rest(): void {
+        if(this.contador >= 1){
+            $(`#boton${this.contador}`).remove();
+             this.contador--;
+        }
+    }
+  }
 
-class Boton {
-    
-}
+  new GrupoBot();
+});
