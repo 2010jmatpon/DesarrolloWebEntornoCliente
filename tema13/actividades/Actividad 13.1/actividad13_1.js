c973ae6ec8081c145d5c72e80fd9271967fccada
2010jmatpon <jmatpon2010@g.educaand.es>
2024-02-14 10:55:00 +0100

    Commit

--- tema13/actividades/Actividad 13.1/actividad13_1.js
+++ tema13/actividades/Actividad 13.1/actividad13_1.js
@@ -1,16 +1,21 @@
 "use strict";
 $(function () {
     class Boton {
-        //solo constructor
+        //solo constructor que irá creando los botones dentro de la zona de contenido además de generar un alert al pulsar en cualquiera de esos botones
         constructor(boton) {
             $('#contenido').append(`<button id="${boton}">${boton}</button>`);
+            $(`#${boton}`).on('click', function () {
+                alert(`Hola desde ${boton}`);
+            });
         }
     }
     class GrupoBot {
         constructor() {
+            //Declaro aquí el contador además de dotar de funcionalidad a los botones de añadir y restar
             this.contador = 0;
             console.log("grupobot");
             $('#add').on('click', () => this.add());
+            $('#rest').on('click', () => this.rest());
         }
         add() {
             this.contador++;
@@ -18,7 +23,7 @@ $(function () {
         }
         rest() {
             if (this.contador >= 1) {
-                $(`#boton${this.contador}`).remove();
+                $(`#${this.contador}`).remove();
                 this.contador--;
             }
         }
--- tema13/actividades/Actividad 13.1/actividad13_1.ts
+++ tema13/actividades/Actividad 13.1/actividad13_1.ts
@@ -1,9 +1,12 @@
 $(function () {
   class Boton {
-    //solo constructor
+    //solo constructor que irá creando los botones dentro de la zona de contenido además de generar un alert al pulsar en cualquiera de esos botones
     constructor(boton:number) {
         $('#contenido').append(`<button id="${boton}">${boton}</button>`);
-    }
+          $(`#${boton}`).on('click', function(){
+              alert(`Hola desde ${boton}`);
+          })
+      }
   }
 
   interface Bot {
@@ -13,10 +16,12 @@ $(function () {
   }
 
   class GrupoBot implements Bot {
+      //Declaro aquí el contador además de dotar de funcionalidad a los botones de añadir y restar
     private contador: number = 0;
     constructor() {
         console.log("grupobot");
         $('#add').on('click',()=>this.add());
+        $('#rest').on('click',()=>this.rest());
     }
     add(): void {
       this.contador++;
@@ -24,7 +29,7 @@ $(function () {
     }
     rest(): void {
         if(this.contador >= 1){
-            $(`#boton${this.contador}`).remove();
+            $(`#${this.contador}`).remove();
              this.contador--;
         }
     }
