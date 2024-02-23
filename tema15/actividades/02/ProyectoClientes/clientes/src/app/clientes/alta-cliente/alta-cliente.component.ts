09e15e0aea3ddead319ca20d6c34102dbd8b79c7
2010jmatpon <jmatpon2010@g.educaand.es>
2024-02-23 10:42:06 +0100

    Commit

---
+++ tema15/actividades/02/ProyectoClientes/clientes/src/app/clientes/alta-cliente/alta-cliente.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ClientesService } from './../clientes.service';
+import { Cliente, Grupo } from './../cliente.model';
+
+@Component({
+  selector: 'app-alta-cliente',
+  templateUrl: './alta-cliente.component.html',
+  styleUrl: './alta-cliente.component.css',
+})
+export class AltaClienteComponent {
+  cliente: Cliente;
+
+  grupos: Grupo[];
+  constructor(private clientesService: ClientesService) {
+    this.cliente = this.clientesService.nuevoCliente();
+    this.grupos = this.clientesService.getGrupos();
+  }
+
+  ngOnInit(){}
+
+  nuevoCliente():void{
+    this.clientesService.agregarCliente(this.cliente);
+    this.cliente = this.clientesService.nuevoCliente();
+
+  }
+}
