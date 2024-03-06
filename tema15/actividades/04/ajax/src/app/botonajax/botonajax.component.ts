756c4284553273d4263e4954af88c73e4b9526d2
2010jmatpon <jmatpon2010@g.educaand.es>
2024-03-06 12:34:13 +0100

    Commit

---
+++ tema15/actividades/04/ajax/src/app/botonajax/botonajax.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ServiajaxService } from '../serviajax.service';
+import {OnInit ,Renderer2 ,ViewChild, ElementRef,} from '@angular/core';
+
+@Component({
+  selector: 'app-botonajax',
+  templateUrl: './botonajax.component.html',
+  styleUrl: './botonajax.component.css'
+})
+
+export class BotonajaxComponent {
+  @ViewChild("datos") idDiv: ElementRef|any;
+
+  constructor(private serviAjax: ServiajaxService, private rd: Renderer2 ) { }
+
+  llamaAjaxC():void{
+    this.serviAjax.llamaAjax('http://localhost/dwec/tema15/actividades/04/ajax/src/app/jsonGET.php?nombre=Juan&ciudad=Ubrique')
+      .subscribe(data=>{
+        this.rd.setProperty(this.idDiv.nativeElement, 'innerHTML',`Desde servidor ${data['nombre']} de ${data['ciudad']}`)
+         
+        }) ;      
+ }
+
+}
